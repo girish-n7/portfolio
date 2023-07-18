4b7176280f369760321adddc3e32c6b454679d4c
Girish_N <ngirish1729@gmail.com>
2023-07-18 15:57:38 +0530

    router changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 // import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
 import SwitchPage from "./components/SwitchPage";
 
 export default function App() {
   return (
     <div>
       <Header />
-      <Router>
-        <SwitchPage />
-      </Router>
+      <SwitchPage />
       <Footer />
     </div>
   );
