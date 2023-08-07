f9c0fe4bff0ef100182cf4ef7ae514266980ce18
Girish_N <ngirish1729@gmail.com>
2023-08-07 22:15:58 +0530

    added download resume button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import Button from "./Button";
 import { TypeAnimation } from "react-type-animation";
+import resume from "../assets/GIRISH N_Resume_web_dev.pdf";
 
 export default function Home() {
   return (
@@ -21,6 +22,16 @@ export default function Home() {
         <p className="home--question">What do you want to do?</p>
         <div className="home--options">
           <Button content="View my projects" route="/projects" />
+          <a
+            href={resume}
+            download="GIRISH N_resume_web_dev.pdf"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="btnHover button home--option">
+              Download my Resume
+            </button>
+          </a>
           <Button content="Know more about me" route="/about" />
           <Button content="Contact me" route="/contact" />
         </div>
